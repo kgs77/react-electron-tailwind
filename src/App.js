b74a74578613b809025bbc78da57dd8334dfc867
kgs77 <sharmaguddu77@gmail.com>
2021-07-04 18:35:02 +0530

    react tailwind css and electron setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="">
+      TEst
+      <h1 className="text-4xl font-bold">Checking react app testing here her</h1>
+      <input className="border-2 border-gray-500" type="text"></input>
+    </div>
+  );
+}
+
+export default App;
